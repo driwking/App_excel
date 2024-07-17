c5cc0dd00d6002092bd99a1a1de50ebee3c024d3
andriw aparecido <andriw1919@gmail.com>
2024-07-17 01:38:39 -0300

    App criado

---
+++ main.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import openpyxl as opx
+import xlsxwriter as xl
+import math
+
+# "arquivo": nome do aquivo excel que será lido
+# "novo": nome do arquivo excel a ser criado  
+config = {"arquivo":"rows0","novo":"modelo"}
+
+# lista de campos para entrarem no novo arquivo
+novasColunas = ["id","patente","valor"]
+
+# lista de campos a serem buscados novos valores, caso tenha espaços no campo do
+# arquivo importante coloca-lo junto, por enquanto.
+colunasDoArquivos = ["cargo","salario","nome"]
+
+def buscarColunas(cols,file):
+    
+    dataFrame = opx.load_workbook(file+".xlsx")
+    dataFrame1 = dataFrame.active
+    posicao = [];valores = []
+    
+    for row in range(0, dataFrame1.max_row):
+        for col in dataFrame1.iter_cols(1,dataFrame1.max_column):
+            if(col[row].value != None and not col[row].value in cols):
+                continue
+            posicao.append(col[row].column)
+        break
+    
+    for row in range(1, dataFrame1.max_row):
+        for col in dataFrame1.iter_cols(1,dataFrame1.max_column):
+            if(col[row].value == None): break
+            if(col[row].column in posicao):
+                valores.append(col[row].value)                
+    
+    dataFrame.close()
+    return valores
+
+def gravarDados(cols,values,file,afects = 0):
+    
+    wb = xl.Workbook(file+".xlsx")
+    ws = wb.add_worksheet()
+    rowPos = 0
+    colPos = 0
+    afects = int(afects)
+    for col in range(0,len(cols)):
+        ws.write(rowPos, col,cols[col])
+    
+    for row in range(1,afects+1):
+        for col in range(0, len(cols)):
+            ws.write(row, col, values[col])
+            
+    wb.close()
+
+if(len(colunasDoArquivos) != len(novasColunas)):  raise Exception("Número de colunas precisam ser igual!")
+
+valoresAserSalvos = buscarColunas(colunasDoArquivos,config.get("arquivo"))
+rowsAfects = math.ceil(len(valoresAserSalvos)/len(colunasDoArquivos)) # linhas totais afetadas não vazias na primeira coluna
+gravarDados(novasColunas,valoresAserSalvos,config.get("novo"),rowsAfects)
+
+# print(valoresAserSalvos) # discomentar para ver os dados capturados
